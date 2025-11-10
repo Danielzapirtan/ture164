711283dbf74efb9f05058dd72d9ca482e4da4b73
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-11-10 20:37:35 +0000

    addcfile

--- ture.c
+++ ture.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int hpd[31];
+#define NDEC (31)
+
+int hpd[NDEC];
 
 void init(void) {
-	for (int i = 0; i < 31; i++)
+	for (int i = 0; i < NDEC; i++)
 		hpd[i] = -1;
+}
+
+int main(void) {
+	int holidays = 11;
+	int holishifts = 4;
+	int workshifts = 9;
+	int blankdays = 11;
 }
 
